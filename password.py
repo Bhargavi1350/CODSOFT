33acd60d3e616bca8232a07d2a96a7f58b1097af
Bhargavi1350 <saibhargavi1350@gmail.com>
2025-08-16 09:05:54 +0000

    Add files via upload

---
+++ password.py
@@ -0,0 +1,25 @@
+import random
+import string
+
+def generate_password(length):
+    if length < 4:
+        print("Password length should be at least 4 characters.")
+        return ""
+    all_chars = string.ascii_letters + string.digits + string.punctuation
+
+    
+    password = ''.join(random.choices(all_chars, k=length))
+    
+    return password
+
+def main():
+    try:
+        length = int(input("Enter the desired password length: "))
+        password = generate_password(length)
+        if password:
+            print("\nGenerated Password:", password)
+    except ValueError:
+        print("Please enter a valid number.")
+
+if __name__ == "__main__":
+    main()
